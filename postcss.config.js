867854090135b781e70b1720a5e18a8024368d70
jhonny stiven agudelo <jhonny.agudelo3120@gmail.com>
2018-05-19 10:03:49 -0500

    Maquetado

---
+++ postcss.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  plugins: [
+    // require('autoprefixer')({
+    //   grid: true
+    // }),
+    require('postcss-import')({
+      plugins: [
+        require('stylelint'),
+      ]
+    }),
+    require('postcss-font-magician')({
+      variants:{
+        'Montserrat':{
+          '300':[],
+          '400':[],
+        }
+      }
+    }),
+    require('postcss-cssnext')({
+      features:{
+        autoprefixer:{
+          grid:true,
+          flexbox:false,
+        },
+        customProperties:false,
+        calc:false,
+      }
+    }),
+    require('css-mqpacker')
+    // require('cssnano')
+  ]
+}
